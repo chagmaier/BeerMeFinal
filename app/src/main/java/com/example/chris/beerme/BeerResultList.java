c1b5f6b94dc700fcd53dff7f5ca7a65c4b85656f
Chris <chagmaier@oxy.edu>
2018-04-30 18:37:40 -0700

    call me bucketboi

--- app/src/main/java/com/example/chris/beerme/BeerResultList.java
+++ app/src/main/java/com/example/chris/beerme/BeerResultList.java
@@ -1,8 +1,10 @@
 package com.example.chris.beerme;
 
+import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
@@ -18,6 +20,7 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Locale;
 
 /**
  * Created by Matt on 4/10/2018.
@@ -28,11 +31,22 @@ public class BeerResultList extends AppCompatActivity {
     private ListView mListView;
     EditText searchBar;
     BeerAdapter adapter;
+    private TextView speakButton;
+    private final int REQ_CODE_SPEECH_INPUT = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.beer_result_view);
+
+        speakButton = (TextView) findViewById(R.id.btnSpeak);
+        speakButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                askSpeechInput();
+            }
+        });
 
         searchBar = findViewById(R.id.search_bar);
         mContext = this;
@@ -90,6 +104,43 @@ public class BeerResultList extends AppCompatActivity {
             }
         });
     }
+
+    // Showing google speech input dialog
+
+    private void askSpeechInput() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
+                "Hi speak something");
+        try {
+            startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
+        } catch (ActivityNotFoundException a) {
+
+        }
+    }
+
+    // Receiving speech input
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case REQ_CODE_SPEECH_INPUT: {
+                if (resultCode == RESULT_OK && null != data) {
+
+                    ArrayList<String> result = data
+                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    searchBar.setText(result.get(0));
+                }
+                break;
+            }
+
+        }
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
